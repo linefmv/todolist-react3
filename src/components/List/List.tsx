df4265a1ae954c4ae896acc578c0f28eb6620ed0
Aline Viana <alinemaciel018@gmail.com>
2021-11-27 14:45:54 -0300

    create list step

---
+++ src/components/List/List.tsx
@@ -0,0 +1,24 @@
+import './list.styles.css'
+import { FiPlus } from 'react-icons/fi'
+const List = () => {
+    return (
+     <>
+    <section className="list">
+         <header>
+             <h2>To do</h2>
+             <div className='input container'> 
+                    <input type='text' placeholder='Add new task' />
+             <button 
+             type='submit'
+             className='add-task' 
+             data-testid='add-task'>
+                 <FiPlus size={16} color='#fff'/> 
+            </button>
+             </div>
+         </header>
+    </section>
+     </>
+    )
+}
+
+export default List
---
+++ src/components/List/list.test.js
@@ -0,0 +1,9 @@
+import {render, screen} from '@testing-library/react'
+import List from './list'
+
+describe('List.jsx', () => { 
+
+    it('Should be able to add a task', () => {
+
+    })
+})
